115e82e25d39293199f5e9895e93b63d241fbf3b
renanmoutaa <renanmoutaa@users.noreply.github.com>
2025-10-20 14:25:23 -0400

    Vite proxy: usar serviços Docker (NestJS, FastAPI) para dev em container

--- vite.config.ts
+++ vite.config.ts
@@ -9,12 +9,12 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000/',
+        target: 'http://NestJS:3000/',
         changeOrigin: true,
         secure: false
       },
       '/pyapi': {
-        target: 'http://localhost:3001/',
+        target: 'http://FastAPI:3001/',
         changeOrigin: true,
         secure: false
       }
